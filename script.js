1e3278094660249650f13a93d985bb3d577a4f25
Chamal1120 <kenway213beep@gmail.com>
2023-08-28 22:18:19 +0530

    project optmization #1

--- script.js
+++ script.js
@@ -1,59 +1,28 @@
-// loading spinner trigger
-const sipnnerWrapperE1 = document.querySelector('.spinner');
+const spinnerWrapperEl = document.querySelector('.spinner');
 
 window.addEventListener('load', () => {
-    sipnnerWrapperE1.style.opacity = '0';
+  spinnerWrapperEl.style.opacity = '0';
 
-    setTimeout(() => {
-        sipnnerWrapperE1.style.display = 'none';
-    }, 200);
-
+  setTimeout(() => {
+    spinnerWrapperEl.style.display = 'none';
+  }, 200);
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-    const hmIcon = document.getElementById("hmIcon");
-    const navLinks = document.querySelector("nav ul");
-    const navLinksList = document.querySelectorAll("nav ul li");
-    const overlay = document.getElementById("overlay");
-  
-    function enableScroll() {
-      document.body.style.overflow = "auto";
-    }
-  
-    function disableScroll() {
-      document.body.style.overflow = "hidden";
-    }
-  
-    hmIcon.addEventListener("click", function() {
-      navLinks.classList.toggle("show");
-      overlay.classList.toggle("active");
-      if (navLinks.classList.contains("show")) {
-        disableScroll();
-      } else {
-        enableScroll();
-      }
-    });
-  
-    navLinksList.forEach(function(link) {
-      link.addEventListener("click", function() {
-        navLinks.classList.remove("show");
-        overlay.classList.remove("active");
-        enableScroll();
-      });
-    });
-  });
-  
-
-
-  
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
+  const hmIcon = document.getElementById("hmIcon");
+  const navLinks = document.querySelector("nav ul");
+  const overlay = document.getElementById("overlay");
   const nav = document.querySelector("nav");
   let prevScrollPos = window.scrollY;
   let isAtTop = true;
 
-  window.addEventListener("scroll", function() {
-    const currentScrollPos = window.scrollY;
+  function toggleScroll() {
+    const shouldScroll = !navLinks.classList.contains("show");
+    document.body.style.overflow = shouldScroll ? "auto" : "hidden";
+  }
 
+  function updateNavBlur() {
+    const currentScrollPos = window.scrollY;
     if (currentScrollPos > prevScrollPos) {
       // Scrolling down
       isAtTop = false;
@@ -65,28 +34,34 @@ document.addEventListener("DOMContentLoaded", function() {
         nav.classList.remove("blurred");
       }
     }
-
     prevScrollPos = currentScrollPos;
+  }
+
+  function updateBackgroundStyles() {
+    const scrollPosition = window.scrollY;
+    const blurValue = Math.min(scrollPosition / 50, 5);
+    const opacityValue = Math.max(0.2 - scrollPosition / 1000, 0);
+    const backgroundImageEl = document.querySelector(".background-image");
+    backgroundImageEl.style.setProperty("--blur-value", blurValue + "px");
+    backgroundImageEl.style.setProperty("--opacity-value", opacityValue);
+  }
+
+  hmIcon.addEventListener("click", function () {
+    navLinks.classList.toggle("show");
+    overlay.classList.toggle("active");
+    toggleScroll();
+  });
+
+  document.querySelectorAll("nav ul li").forEach(function (link) {
+    link.addEventListener("click", function () {
+      navLinks.classList.remove("show");
+      overlay.classList.remove("active");
+      toggleScroll();
+    });
+  });
+
+  window.addEventListener("scroll", () => {
+    updateNavBlur();
+    updateBackgroundStyles();
   });
 });
-
-
-document.addEventListener("DOMContentLoaded", function () {
-    document.addEventListener("scroll", function () {
-      const scrollPosition = window.scrollY;
-      const blurValue = Math.min(scrollPosition / 50, 5);
-      const opacityValue = Math.max(0.2 - scrollPosition / 1000, 0);
-
-      document.querySelector(".background-image").style.setProperty("--blur-value", blurValue + "px");
-      document.querySelector(".background-image").style.setProperty("--opacity-value", opacityValue);
-    });
-});
-
-
-
-
-
-
-  
-
-
