51231ebe4df90e18441c0337bdfa2dca6bcedebd
Chamal1120 <kenway213beep@gmail.com>
2023-09-08 17:09:19 +0530

    Carousel bug fix #1

--- slickScript.js
+++ slickScript.js
@@ -1,14 +1,17 @@
 $(document).ready(function () {
     // Initialize Slick Carousel for each slider
     $('.slider').slick({
+        lazyLoad: 'ondemand',
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 4000,
-        swipe: true,
+        autoplaySpeed: 3000,
+        swipe: false,
         dots: false,
         arrows: false,
-        speed: 2000,
-        easing: 'ease-in-out',
+        speed: 1500,
+        fade: true,
+        cssEase: 'ease-in-out',
+        infinite: true
     });
 });
